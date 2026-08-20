ced1003437144bb687104a471a50ed66d9acbfed
Charles Lawrence <charleslawrence@Charless-iMac.home.local>
2026-08-20 07:40:47 -0500

    Lock version to 1.0.7 matching PyPI release

--- psas_swarm/__init__.py
+++ psas_swarm/__init__.py
@@ -1,6 +1,6 @@
 <<<<<<< HEAD
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 =======
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 from .core import PhaseShiftedAgentRouter
 >>>>>>> 6d3109cf69603a782dd679aaf923b47a58355b2a
